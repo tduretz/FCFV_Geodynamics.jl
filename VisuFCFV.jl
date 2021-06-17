7fe51d1a93dca548cc90661586c42bf04996da14
Ludovic Räss <ludovic.rass@gmail.com>
2021-06-17 16:05:39 +0200

    fix lims for visu

--- VisuFCFV.jl
+++ VisuFCFV.jl
@@ -4,15 +4,18 @@ using Makie.GeometryBasics
 @views function PlotMakie( mesh, v )
     ``` Create patch plots for either Quadrangles or Triangles
     ```
+
+    min_v, max_v = minimum(v), maximum(v)
+
     f = Figure()
 
     Axis(f[1, 1])
 
     p = [Polygon( Point2f0[ (mesh.xv[mesh.e2v[i,j]], mesh.yv[mesh.e2v[i,j]]) for j=1:mesh.nf_el] ) for i in 1:mesh.nel]
 
-    poly!(p, color = v, colormap = :viridis, strokewidth = 0, strokecolor = :black, markerstrokewidth = 0, markerstrokecolor = (0, 0, 0, 0), aspect_ratio = :equal, clims=[0.7 1.4])
+    poly!(p, color = v, colormap = :viridis, strokewidth = 0, strokecolor = :black, markerstrokewidth = 0, markerstrokecolor = (0, 0, 0, 0), aspect_ratio = :equal, clims=[min_v max_v])
 
-    Colorbar(f[1, 2], colormap = :viridis, limits=[0.7 1.4], flipaxis = true, size = 25, height = Relative(2/3) )
+    Colorbar(f[1, 2], colormap = :viridis, limits=[min_v max_v], flipaxis = true, size = 25, height = Relative(2/3) )
 
     xlims!(0, 1)
     ylims!(0, 1)
