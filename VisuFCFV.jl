5ae49b098c2852bde5899e18c8094dc376c90bc6
Ludovic Räss <ludovic.rass@gmail.com>
2021-06-17 22:22:03 +0200

    update visu with kwargs fix color limit

--- VisuFCFV.jl
+++ VisuFCFV.jl
@@ -1,11 +1,11 @@
 using GLMakie
 using Makie.GeometryBasics
 
-@views function PlotMakie( mesh, v, xmin, xmax, ymin, ymax, cmap, cblims )
+@views function PlotMakie(mesh, v, xmin, xmax, ymin, ymax; cmap = :viridis, min_v = minimum(v), max_v = maximum(v))
     ``` Create patch plots for either Quadrangles or Triangles
     ```
 
-    min_v, max_v = minimum(v), maximum(v)
+    # min_v, max_v = minimum(v), maximum(v)
 
     f = Figure()
 
@@ -13,14 +13,9 @@ using Makie.GeometryBasics
 
     p = [Polygon( Point2f0[ (mesh.xv[mesh.e2v[i,j]], mesh.yv[mesh.e2v[i,j]]) for j=1:mesh.nf_el] ) for i in 1:mesh.nel]
 
-    poly!(p, color = v, colormap = cmap, strokewidth = 0, strokecolor = :black, markerstrokewidth = 0, markerstrokecolor = (0, 0, 0, 0), aspect_ratio = :equal, clims=cblims)
+    poly!(p, color = v, colormap = cmap, strokewidth = 0, strokecolor = :black, markerstrokewidth = 0, markerstrokecolor = (0, 0, 0, 0), aspect_ratio = :equal, colorrange=(min_v,max_v))
 
-    Colorbar(f[1, 2], colormap = cmap, limits=cblims, flipaxis = true, size = 25, height = Relative(2/3) )
-    
-    # poly!(p, color = v, colormap = cmap, strokewidth = 0, strokecolor = :black, markerstrokewidth = 0, markerstrokecolor = (0, 0, 0, 0), aspect_ratio = :equal, clims=cblims)
-
-    # Colorbar(f[1, 2], colormap = cmap, limits=cblims, flipaxis = true, size = 25, height = Relative(2/3) )
-
+    Colorbar(f[1, 2], colormap = cmap, limits=[min_v max_v], flipaxis = true, size = 25, height = Relative(2/3) )
 
     xlims!(xmin, xmax)
     ylims!(ymin, ymax)
