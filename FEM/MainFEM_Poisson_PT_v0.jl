6306c40a95c7028a9c8ad18e79e834c09ee8ce31
Duretz Thibault <thibault.duretz@univ-rennes1.fr>
2022-06-12 15:42:31 +0200

    Added Element-based FEM residual

--- FEM/MainFEM_Poisson_PT_v0.jl
+++ FEM/MainFEM_Poisson_PT_v0.jl
@@ -1,5 +1,6 @@
-const USE_GPU = false # ACHTUNG -> uncomment PT loop !!
+const USE_GPU    = false  # not supported yet 
 const USE_DIRECT = false
+const USE_NODAL  = false
 
 using Printf, LoopVectorization, LinearAlgebra, SparseArrays, MAT
 
@@ -8,21 +9,25 @@ include("../VisuFCFV.jl")
 include("../DiscretisationFCFV.jl")
 include("IntegrationPoints.jl")
 
+#----------------------------------------------------------#
+
 function StabParam(τ, Γ, Ω, mesh_type, ν)
     return 0.
 end
 
-function ResidualPoissonFEM!( F, T, mesh, K_all, b )
-     # Residual
+#----------------------------------------------------------#
+
+function ResidualPoissonNodalFEM!( F, T, mesh, K_all, b )
+    # Residual
     @inbounds for in = 1:mesh.nv
         F[in] = 0.0
         if mesh.bcn[in]==0
             for ii=1:length(mesh.n2e[in])
-                el      = mesh.n2e[in][ii]
-                nodes   = mesh.e2v[el,:]
-                Tel     = T[nodes]  
-                K       = K_all[el,:,:] 
-                f       = K*Tel .- b[el]
+                e       = mesh.n2e[in][ii]
+                nodes   = mesh.e2v[e,:]
+                T_ele   = T[nodes]  
+                K       = K_all[e,:,:] 
+                f       = K*T_ele .- b[e]
                 inode   = mesh.n2e_loc[in][ii]
                 F[in]  -= f[inode]
             end
@@ -30,6 +35,24 @@ function ResidualPoissonFEM!( F, T, mesh, K_all, b )
     end
     return nothing
 end
+
+#----------------------------------------------------------#
+
+function ResidualPoissonElementalFEM!( F, T, mesh, K_all, b )
+    # Residual
+    F .= 0.0
+    @inbounds for e = 1:mesh.nel
+        nodes     = mesh.e2v[e,:]
+        T_ele      = T[nodes]
+        K_ele      = K_all[e,:,:]
+        f_ele      = K_ele*T_ele .- b[e,:]
+        F[nodes] .-= f_ele[:] # This should be atomic
+    end
+    F[mesh.bcn.==1] .= 0.0
+    return nothing
+end
+
+#----------------------------------------------------------#
 
 function ElementAssemblyLoopFEM( se, mesh, ipw, N, dNdX ) 
 
@@ -41,10 +64,10 @@ function ElementAssemblyLoopFEM( se, mesh, ipw, N, dNdX )
     b_ele        = zeros(nnel)
 
     # Element loop
-    @inbounds for el = 1:mesh.nel
-        nodes   = mesh.e2v[el,:]
+    @inbounds for e = 1:mesh.nel
+        nodes   = mesh.e2v[e,:]
         x       = [mesh.xv[nodes] mesh.yv[nodes]]
-        ke      = mesh.ke[el]
+        ke      = mesh.ke[e]
         J       = zeros(2,2)
         invJ    = zeros(2,2)
         K_ele  .= 0.0
@@ -60,13 +83,15 @@ function ElementAssemblyLoopFEM( se, mesh, ipw, N, dNdX )
             invJ[2,2] = +J[1,1] / detJ
             dNdx      = dNdXi*invJ
             K_ele   .+= ipw[ip] .* detJ .* ke .* (dNdx*dNdx')
-            b_ele   .+= ipw[ip] .* detJ .* se[el] .* N[ip,:] 
+            b_ele   .+= ipw[ip] .* detJ .* se[e] .* N[ip,:] 
         end
-        K_all[el,:,:] .= K_ele
-        b[el,:]       .= b_ele
+        K_all[e,:,:] .= K_ele
+        b[e,:]       .= b_ele
     end
     return K_all, b
 end 
+
+#----------------------------------------------------------#
 
 function DirectSolveFEM!(T, mesh, K_all, b)
 
@@ -74,27 +99,33 @@ function DirectSolveFEM!(T, mesh, K_all, b)
     K    = sparse(Int64[], Int64[], Float64[], ndof, ndof)
     rhs  = zeros(ndof)
 
-    for el=1:mesh.nel
-        nodes   = mesh.e2v[el,:]
+    # Assembly of global sparse matrix
+    @inbounds for e=1:mesh.nel
+        nodes = mesh.e2v[e,:]
         for j=1:3
             if mesh.bcn[nodes[j]] == 0
-                # If not Dirichlet
+                # If not Dirichlet, add connection
                 for i=1:3  
-                    K[nodes[j], nodes[i]] += K_all[el,j,i]
+                    if mesh.bcn[nodes[i]] == 0 # Important to keep matrix symmetric
+                        K[nodes[j], nodes[i]] += K_all[e,j,i]
+                    else
+                        rhs[nodes[j]] -= K_all[e,j,i]*T[nodes[i]]
+                    end
                 end
-                rhs[nodes[j]] += b[el,j]
+                rhs[nodes[j]] += b[e,j]
             else
-                # Deal with Dirichlet
+                # Deal with Dirichlet: set one on diagonal and value and RHS
                 rhs[nodes[j]]         = T[nodes[j]]
                 K[nodes[j], nodes[j]] = 1.0
             end
         end 
     end
-    
-    T .= K\rhs
-
+    # Solve using CHOLDMOD
+    T .= cholesky(K)\rhs
     return nothing
 end
+
+#----------------------------------------------------------#
 
 function main( n, θ, Δτ )
 
@@ -105,13 +136,10 @@ function main( n, θ, Δτ )
     nx, ny     = Int16(n*20), Int16(n*20)
     R          = 0.5
     inclusion  = 0
-    direct     = true 
-
-     # mesh_type  = "Quadrangles"
-    mesh_type  = "UnstructTriangles"
+    mesh_type  = "UnstructTriangles" 
   
     # Generate mesh
-    if mesh_type=="Quadrangles" 
+    if mesh_type=="Quadrangles" # not supported for FEM
         τr   = 1
         mesh = MakeQuadMesh( nx, ny, xmin, xmax, ymin, ymax, τr, inclusion, R )
     elseif mesh_type=="UnstructTriangles"  
@@ -121,9 +149,8 @@ function main( n, θ, Δτ )
     println("Number of elements: ", mesh.nel)
     println("Number of vertices: ", mesh.nv)
 
-    T  = zeros(mesh.nv)
-    F  = zeros(mesh.nv)
-    se = zeros(mesh.nel)
+    T  = zeros(mesh.nv)     # Solution on nodes 
+    se = zeros(mesh.nel)    # Source term on elements
 
     # Intial guess
     alp = 0.1; bet = 0.3; a = 5.1; b = 4.3; c = -6.2; d = 3.4;
@@ -136,13 +163,14 @@ function main( n, θ, Δτ )
     end
 
     # Evaluate T analytic on barycentres
-    for el=1:mesh.nel
-        x      = mesh.xc[el]
-        y      = mesh.yc[el]
+    for e=1:mesh.nel
+        x      = mesh.xc[e]
+        y      = mesh.yc[e]
         Te     = exp(alp*sin(a*x + c*y) + bet*cos(b*x + d*y))
-        se[el] = Te*(-a*alp*cos(a*x + c*y) + b*bet*sin(b*x + d*y))*(a*alp*cos(a*x + c*y) - b*bet*sin(b*x + d*y)) + Te*(a^2*alp*sin(a*x + c*y) + b^2*bet*cos(b*x + d*y)) + Te*(-alp*c*cos(a*x + c*y) + bet*d*sin(b*x + d*y))*(alp*c*cos(a*x + c*y) - bet*d*sin(b*x + d*y)) + Te*(alp*c^2*sin(a*x + c*y) + bet*d^2*cos(b*x + d*y))
+        se[e] = Te*(-a*alp*cos(a*x + c*y) + b*bet*sin(b*x + d*y))*(a*alp*cos(a*x + c*y) - b*bet*sin(b*x + d*y)) + Te*(a^2*alp*sin(a*x + c*y) + b^2*bet*cos(b*x + d*y)) + Te*(-alp*c*cos(a*x + c*y) + bet*d*sin(b*x + d*y))*(alp*c*cos(a*x + c*y) - bet*d*sin(b*x + d*y)) + Te*(alp*c^2*sin(a*x + c*y) + bet*d^2*cos(b*x + d*y))
     end
 
+    # Element data
     nip  = 3
     nnel = 3
     ipx, ipw = IntegrationTriangle( nip )
@@ -151,8 +179,8 @@ function main( n, θ, Δτ )
     #-----------------------------------------------------------------#
     K_all, b = ElementAssemblyLoopFEM( se, mesh, ipw, N, dNdX )
     #-----------------------------------------------------------------#
-    if direct
-        DirectSolveFEM!(T, mesh, K_all, b)
+    if USE_DIRECT
+        @time DirectSolveFEM!(T, mesh, K_all, b)
     else
         nout    = 50#1e1
         iterMax = 2e3#5e4
@@ -168,17 +196,19 @@ function main( n, θ, Δτ )
         # Δx = minimum(mesh.Γ)
         # D  = 1.0
         # println("Δτ1 = ", Δx^2/(1.1*D) * 1.0/Ωe *2/3)
-
-        ΔTΔτ  = zeros(mesh.nv)
+        ΔTΔτ  = zeros(mesh.nv) # Residual
         ΔTΔτ0 = zeros(mesh.nv)
-
         # PT loop
         local iter = 0
         success = 0
         @time while (iter<iterMax)
             iter +=1
             ΔTΔτ0 .= ΔTΔτ 
-            ResidualPoissonFEM!( ΔTΔτ, T, mesh, K_all, b )
+            if USE_NODAL
+                ResidualPoissonNodalFEM!( ΔTΔτ, T, mesh, K_all, b )
+            else
+                ResidualPoissonElementalFEM!( ΔTΔτ, T, mesh, K_all, b )
+            end
             ΔTΔτ  .= (1.0 - θ).*ΔTΔτ0 .+ ΔTΔτ 
             T    .+= Δτ .* ΔTΔτ
             if iter % nout == 0 || iter==1
@@ -203,19 +233,19 @@ function main( n, θ, Δτ )
 
     #-----------------------------------------------------------------#
 
-    T_ele = zeros(mesh.nel)
-    for el=1:mesh.nel
+    Te = zeros(mesh.nel)
+    for e=1:mesh.nel
         for in=1:3
-            T_ele[el] += 1.0/3.0 * T[mesh.e2v[el,in]]
+            Te[e] += 1.0/3.0 * T[mesh.e2v[e,in]]
         end
     end
 
-    PlotMakie(mesh, T_ele, xmin, xmax, ymin, ymax)
+    PlotMakie(mesh, Te, xmin, xmax, ymin, ymax)
    
 end
 
 # main(1, 0.20398980000000003,        0.23333333333333336) # 150
-# main(2, 0.20398980000000003*0.61,   0.23333333333333336) # 250
+main(2, 0.20398980000000003*0.61,   0.23333333333333336) # 250
 # main(4, 0.20398980000000003*0.61/2, 0.23333333333333336*0.96) # 500
-main(8, 0.20398980000000003*0.61/4 * 0.98, 0.23333333333333336*0.88) # 1000
+# main(8, 0.20398980000000003*0.61/4 * 0.98, 0.23333333333333336*0.88) # 1000
 
