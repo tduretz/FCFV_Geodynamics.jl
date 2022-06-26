4aaf2840ddb7e972af563756c4a6d1bed7ebf21f
Duretz Thibault <thibault.duretz@univ-rennes1.fr>
2022-06-26 14:34:11 +0200

    Add news test with gravity, mini still not working

---
+++ FEM/MainFEM_Stokes_PT_v2_Sevilla2018.jl
@@ -0,0 +1,465 @@
+const USE_GPU      = false  # Not supported yet 
+const USE_DIRECT   = true   # Sparse matrix assembly + direct solver
+const USE_NODAL    = false  # Nodal evaluation of residual
+const USE_PARALLEL = false  # Parallel residual evaluation
+const USE_MAKIE    = true   # Visualisation 
+import Plots
+
+using Printf, LoopVectorization, LinearAlgebra, SparseArrays, MAT, StaticArrays
+import Base.Threads: @threads, @sync, @spawn, nthreads, threadid
+using MAT
+
+include("../CreateMeshFCFV.jl")
+include("../VisuFCFV.jl")
+# include("../EvalAnalDani.jl")
+include("IntegrationPoints.jl")
+
+#----------------------------------------------------------#
+
+function StabParam(τ, Γ, Ω, mesh_type, ν) 
+    return 0. # Stabilisation is only needed for FCFV
+end
+
+#----------------------------------------------------------#
+
+function EvalSolSevilla2018( x, y )
+    p   =  x*(1-x)
+    Vx  =  x^2*(1 - x)^2*(4*y^3 - 6*y^2 + 2*y)
+    Vy  = -y^2*(1 - y)^2*(4*x^3 - 6*x^2 + 2*x)
+    Sxx = -8*p*y*(x - 1)*(2*y^2 - 3*y + 1) - p + 8*x^2*y*(x - 1)*(2*y^2 - 3*y + 1)
+    Syy = -p - 8*x*y^2*(y - 1)*(2*x^2 - 3*x + 1) - 8*x*y*(y - 1)^2*(2*x^2 - 3*x + 1)
+    Sxy = p^2*(12*y^2 - 12*y + 2) + y^2*(y - 1)^2*(-12.0*x^2 + 12.0*x - 2.0)
+    sx  = -p^2*(24*y - 12) - 4*x^2*(4*y^3 - 6*y^2 + 2*y) - 8*x*(2*x - 2)*(4*y^3 - 6*y^2 + 2*y) - 2*x + 1.0*y^2*(2*y - 2)*(12*x^2 - 12*x + 2) + 2.0*y*(1 - y)^2*(12*x^2 - 12*x + 2) - 4*(1 - x)^2*(4*y^3 - 6*y^2 + 2*y) + 1
+    sy  = -2*p*(1 - x)*(12*y^2 - 12*y + 2) - x^2*(2*x - 2)*(12*y^2 - 12*y + 2) + 1.0*y^2*(1 - y)^2*(24*x - 12) + 4*y^2*(4*x^3 - 6*x^2 + 2*x) + 8*y*(2*y - 2)*(4*x^3 - 6*x^2 + 2*x) + 4*(1 - y)^2*(4*x^3 - 6*x^2 + 2*x)
+    return Vx, Vy, p, Sxx, Syy, Sxy, sx, sy
+end
+
+#----------------------------------------------------------#
+
+function ResidualStokesNodalFEM!( Fx, Fy, Fp, Vx, Vy, P, mesh, K_all, Q_all, b )
+    # Residual
+    Fx   .= 0.0
+    Fy   .= 0.0
+    Fp   .= 0.0
+    nnel  = mesh.nnel
+    npel  = mesh.npel
+    ###################################### VELOCITY
+    # Loop over nodes and elements connected to each node to avoid race condition - quite horrible
+    Threads.@threads for in = 1:mesh.nn
+        Fx[in] = 0.0
+        Fy[in] = 0.0
+        if mesh.bcn[in]==0
+            for ii=1:length(mesh.n2e[in])
+                e       = mesh.n2e[in][ii]
+                nodes   = mesh.e2n[e,:]
+                if npel==1 nodesP = [e] end
+                if npel==3 nodesP = [e; e+mesh.nel; e+2*mesh.nel]  end
+                V_ele             = zeros(nnel*2)
+                V_ele[1:2:end-1] .= Vx[nodes]
+                V_ele[2:2:end]   .= Vy[nodes]  
+                P_ele      = P[nodesP] 
+                K_ele      = K_all[e,:,:]
+                Q_ele      = Q_all[e,:,:]
+                b_ele      = b_all[e,:]
+                fv_ele     = K_ele*V_ele .+ Q_ele*P_ele .- b_ele
+                inode      = mesh.n2e_loc[in][ii]
+                Fx[in]    -= fv_ele[2*inode-1]
+                Fy[in]    -= fv_ele[2*inode]
+            end
+        end
+    end
+    ###################################### PRESSURE
+    # Pressure is discontinuous across elements, the residual can be evaluated per element without race condition
+    V_ele = zeros(nnel*2)
+    @inbounds for e = 1:mesh.nel
+        Fp[e] = 0.0
+        nodes  = mesh.e2n[e,:]
+        nodesP = mesh.e2p[e,:]
+        V_ele[1:2:end-1] .= Vx[nodes]
+        V_ele[2:2:end]   .= Vy[nodes]
+        Q_ele      = Q_all[e,:,:]
+        fp_ele     = .-Q_ele'*V_ele #.- bp[e,:]
+        for p=1:npel
+            Fp[e] = - fp_ele[:][p]
+        end
+    end
+    return nothing
+end
+
+#----------------------------------------------------------#
+
+function ResidualStokesElementalSerialFEM!( Fx, Fy, Fp, Vx, Vy, P, mesh, K_all, Q_all, b_all )
+    # Residual
+    Fx   .= 0.0
+    Fy   .= 0.0
+    Fp   .= 0.0
+    nnel  = mesh.nnel
+    npel  = mesh.npel
+    V_ele = zeros(nnel*2)
+    b_ele = zeros(nnel*2)
+    @inbounds for e = 1:mesh.nel
+        nodes  = mesh.e2n[e,:]
+        nodesP = mesh.e2p[e,:]
+        V_ele[1:2:end-1] .= Vx[nodes]
+        V_ele[2:2:end]   .= Vy[nodes]
+        P_ele      = P[nodesP] 
+        K_ele      = K_all[e,:,:]
+        Q_ele      = Q_all[e,:,:]
+        b_ele      = b_all[e,:]
+        fv_ele     = K_ele*V_ele .+ Q_ele*P_ele .- b_ele
+        fp_ele     = .-Q_ele'*V_ele #.- bp[e,:]
+        Fx[nodes] .-= fv_ele[1:2:end-1] # This should be atomic
+        Fy[nodes] .-= fv_ele[2:2:end]
+        if npel==1 Fp[nodesP]  -= fp_ele[:] end
+        if npel==3 Fp[nodesP] .-= fp_ele[:] end
+    end
+    Fx[mesh.bcn.==1] .= 0.0
+    Fy[mesh.bcn.==1] .= 0.0
+    return nothing
+end
+
+#----------------------------------------------------------#
+
+function ElementAssemblyLoopFEM( se, mesh, ipx, ipw, N, dNdX ) # Adapted from MILAMIN_1.0.1
+    ndof         = 2*mesh.nnel
+    nnel         = mesh.nnel
+    npel         = mesh.npel
+    nip          = length(ipw)
+    K_all        = zeros(mesh.nel, ndof, ndof) 
+    Q_all        = zeros(mesh.nel, ndof, npel)
+    Mi_all       = zeros(mesh.nel, npel, npel)
+    b_all        = zeros(mesh.nel, ndof )
+    K_ele        =  zeros(ndof, ndof)
+    Q_ele        =  zeros(ndof, npel)
+    M_ele        =  zeros(npel ,npel)
+    M_inv        =  zeros(npel ,npel)
+    b_ele        =  zeros(ndof)
+    B            =  zeros(ndof, 3)
+    m            =  [ 1.0; 1.0; 0.0]
+    Dev          =  [ 4/3 -2/3  0.0;
+                             -2/3  4/3  0.0;
+                              0.0  0.0  1.0]
+    P  = ones(npel,npel)
+    Pb = ones(npel)
+    # PF = 1e3*maximum(mesh.ke)
+    # Np0 = N[:,1:3,:]
+    Np0, dNdXp   = ShapeFunctions(ipx, nip, 3)
+    # if nnel==4 # load linear basis function for pressure (3 nodes)
+    #     Np0, dNdXp   = ShapeFunctions(ipx, nip, 3)
+    # end 
+    
+    # display(N)
+    # display(Np0)
+
+    # Element loop
+    @inbounds for e = 1:mesh.nel
+        nodes   = mesh.e2n[e,:]
+        x       = [mesh.xn[nodes] mesh.yn[nodes]]  
+        ke      = mesh.ke[e]
+        J       = zeros(2,2)
+        invJ    = zeros(2,2)
+        K_ele  .= 0.0
+        Q_ele  .= 0.0
+        M_ele  .= 0.0
+        b_ele  .= 0.0
+        M_inv  .= 0.0
+        if npel==3 && nnel!=4 P[2:3,:] .= (x[1:3,:])' end
+
+        # Integration loop
+        for ip=1:nip
+
+            dNdXi     = dNdX[ip,:,:]
+            J        .= x'*dNdXi
+            detJ      = J[1,1]*J[2,2] - J[1,2]*J[2,1]
+            invJ[1,1] = +J[2,2] / detJ
+            invJ[1,2] = -J[1,2] / detJ
+            invJ[2,1] = -J[2,1] / detJ
+            invJ[2,2] = +J[1,1] / detJ
+            dNdx      = dNdXi*invJ
+            B[1:2:end,1] .= dNdx[:,1]
+            B[2:2:end,2] .= dNdx[:,2]
+            B[1:2:end,3] .= dNdx[:,2]
+            B[2:2:end,3] .= dNdx[:,1]
+            Bvol          = dNdx'
+            K_ele .+= ipw[ip] .* detJ .* ke  .* (B*Dev*B')
+            if npel==3 && nnel!=4 
+                Np       = N[ip,:,1]
+                Pb[2:3] .= x'*Np 
+                Pi       = P\Pb
+                Q_ele   .-= ipw[ip] .* detJ .* (Bvol[:]*Pi') 
+                # M_ele   .+= ipw[ip] .* detJ .* Pi*Pi' # mass matrix P, not needed for incompressible
+            else
+                if npel==1 Np = 1.0        end
+                if npel==3 Np = Np0[ip,:,:] end
+                Q_ele   .-= ipw[ip] .* detJ .* (B*m*Np') 
+            end
+            b_ele[1:2:end] .+= ipw[ip] .* detJ .* se[e,1] .* N[ip,:] 
+            b_ele[2:2:end] .+= ipw[ip] .* detJ .* se[e,2] .* N[ip,:]
+        end
+        K_all[e,:,:]  .= K_ele
+        Q_all[e,:,:]  .= Q_ele
+        b_all[e,:]    .= b_ele
+    end
+    return K_all, Q_all, Mi_all, b_all
+end 
+
+#----------------------------------------------------------#
+
+function SparseAssembly( K_all, Q_all, Mi_all, b_all, mesh, Vx, Vy, P )
+
+    println("Assembly")
+    ndof = mesh.nn*2
+    npel = mesh.npel
+    rhs  = zeros(ndof+mesh.np)
+    I_K  = Int64[]
+    J_K  = Int64[]
+    V_K  = Float64[]
+    I_Q  = Int64[]
+    J_Q  = Int64[]
+    V_Q  = Float64[]
+    I_Qt = Int64[]
+    J_Qt = Int64[]
+    V_Qt = Float64[]
+    I_M  = Int64[]
+    J_M  = Int64[]
+    V_M  = Float64[]
+
+    # Assembly of global sparse matrix
+    @inbounds for e=1:mesh.nel
+        nodes   = mesh.e2n[e,:]
+        nodesVx = mesh.e2n[e,:]
+        nodesVy = nodesVx .+ mesh.nn 
+        nodesP  = mesh.e2p[e,:]
+        jj = 1
+        for j=1:mesh.nnel
+
+            # Q: ∇ operator: BC for V equations
+            if mesh.bcn[nodes[j]] != 1
+                for i=1:npel
+                    push!(I_Q, nodesVx[j]); push!(J_Q, nodesP[i]); push!(V_Q, Q_all[e,jj  ,i])
+                    push!(I_Q, nodesVy[j]); push!(J_Q, nodesP[i]); push!(V_Q, Q_all[e,jj+1,i])
+                end
+            end
+
+            # Qt: ∇⋅ operator: no BC for P 
+        # if mesh.bcn[nodes[j]] != 1
+            for i=1:npel
+                push!(J_Qt, nodesVx[j]); push!(I_Qt, nodesP[i]); push!(V_Qt, Q_all[e,jj  ,i])
+                push!(J_Qt, nodesVy[j]); push!(I_Qt, nodesP[i]); push!(V_Qt, Q_all[e,jj+1,i])
+            end
+        # end
+
+            if mesh.bcn[nodes[j]] != 1
+                # If not Dirichlet, add connection
+                ii = 1
+                for i=1:mesh.nnel
+                    if mesh.bcn[nodes[i]] != 1 # Important to keep matrix symmetric
+                        push!(I_K, nodesVx[j]); push!(J_K, nodesVx[i]); push!(V_K, K_all[e,jj,  ii  ])
+                        push!(I_K, nodesVx[j]); push!(J_K, nodesVy[i]); push!(V_K, K_all[e,jj,  ii+1])
+                        push!(I_K, nodesVy[j]); push!(J_K, nodesVx[i]); push!(V_K, K_all[e,jj+1,ii  ])
+                        push!(I_K, nodesVy[j]); push!(J_K, nodesVy[i]); push!(V_K, K_all[e,jj+1,ii+1])
+                    else
+                        rhs[nodesVx[j]] -= K_all[e,jj  ,ii  ]*Vx[nodes[i]]
+                        rhs[nodesVx[j]] -= K_all[e,jj  ,ii+1]*Vy[nodes[i]]
+                        rhs[nodesVy[j]] -= K_all[e,jj+1,ii  ]*Vx[nodes[i]]
+                        rhs[nodesVy[j]] -= K_all[e,jj+1,ii+1]*Vy[nodes[i]]
+                    end
+                    ii+=2
+                end
+                rhs[nodesVx[j]] += b_all[e,jj]
+                rhs[nodesVy[j]] += b_all[e,jj+1]
+            else
+                # Deal with Dirichlet: set one on diagonal and value and RHS
+                push!(I_K, nodesVx[j]); push!(J_K, nodesVx[j]); push!(V_K, 1.0)
+                push!(I_K, nodesVy[j]); push!(J_K, nodesVy[j]); push!(V_K, 1.0)
+                rhs[nodesVx[j]] += Vx[nodes[j]]
+                rhs[nodesVy[j]] += Vy[nodes[j]]
+            end
+            jj+=2
+            if mesh.nnel==4 && mesh.npel==3 # only for mini-element
+                for j=1:mesh.npel
+                    if mesh.bcn[nodes[j]] == 1
+                        push!(I_M, nodesP[j]); push!(J_M, nodesP[j]); push!(V_M, -1.0)
+                        rhs[nodesP[j]] += P[nodes[j]]
+                    end
+                end
+            end
+        end 
+    end
+    K  = sparse(I_K,  J_K,  V_K, ndof, ndof)
+    Q  = sparse(I_Q,  J_Q,  V_Q, ndof, mesh.np)
+    Qt = sparse(I_Qt, J_Qt, V_Qt, mesh.np, ndof)
+    M0 = sparse(I_M,  J_M,  V_M, mesh.np, mesh.np)
+    M  = [K Q; Qt M0]
+    return M, rhs, K, Q, Qt, M0
+end
+
+#----------------------------------------------------------#
+
+function DirectSolveFEM!( M, K, Q, Qt, M0, rhs, Vx, Vy, P, mesh, b)
+    println("Direct solve")
+    ndof       = mesh.nn*2
+    sol        = zeros(ndof+mesh.np)
+    # M[end,:]  .= 0.0 # add one Dirichlet constraint on pressure
+    # M[end,end] = 1.0
+    # rhs[end]   = 0.0
+    sol       .= M\rhs
+    Vx        .= sol[1:length(Vx)] 
+    Vy        .= sol[(length(Vx)+1):(length(Vx)+length(Vy))]
+    P         .= sol[(ndof+1):end]
+    return nothing
+end
+
+#----------------------------------------------------------#
+
+function main( n, nnel, npel, nip, θ, ΔτV, ΔτP )
+
+    println("\n******** FEM STOKES ********")
+    # Create sides of mesh
+    xmin, xmax = -.0, 1.0
+    ymin, ymax = -.0, 1.0
+    nx, ny     = Int16(n*30), Int16(n*30)
+    R          = 1.0
+    inclusion  = 0
+    εBG        = 1.0
+    η          = [1.0 5.0]  
+    
+    # Element data
+    ipx, ipw  = IntegrationTriangle(nip)
+    N, dNdX   = ShapeFunctions(ipx, nip, nnel)
+  
+    # Generate mesh
+    mesh = MakeTriangleMesh( nx, ny, xmin, xmax, ymin, ymax, 0.0, inclusion, R; nnel, npel ) 
+    println("Number of elements: ", mesh.nel)
+    println("Number of vertices: ", mesh.nn)
+    println("Number of p nodes:  ", mesh.np)
+
+    Vx  = zeros(mesh.nn)       # Solution on nodes 
+    Vy  = zeros(mesh.nn)       # Solution on nodes 
+    P   = zeros(mesh.np)       # Solution on either elements or nodes 
+    se  = zeros(mesh.nel)      # Source term on elements
+    Pa  = zeros(mesh.nel)      # Solution on elements 
+    se  = zeros(mesh.nel,2)    # Source on elements 
+
+    # Intial guess
+    for in = 1:mesh.nn
+        if mesh.bcn[in]==1
+            x      = mesh.xn[in]
+            y      = mesh.yn[in]
+            vx, vy, p, Sxx, Syy, Sxy, sx, sy = EvalSolSevilla2018( x, y )
+            Vx[in] = vx
+            Vy[in] = vy
+        end
+    end
+    for e = 1:mesh.nel
+        x      = mesh.xc[e]
+        y      = mesh.yc[e]
+        vx, vy, p, Sxx, Syy, Sxy, sx, sy = EvalSolSevilla2018( x, y )
+        Pa[e]   = p
+        se[e,1] = sx
+        se[e,2] = sy
+    end
+    for in = 1:mesh.nv
+        if mesh.bcn[in]==1
+            x      = mesh.xn[in]
+            y      = mesh.yn[in]
+            vx, vy, p, Sxx, Syy, Sxy, sx, sy = EvalSolSevilla2018( x, y )
+            P[in] = p
+        end
+    end
+
+    #-----------------------------------------------------------------#
+    @time  K_all, Q_all, Mi_all, b_all = ElementAssemblyLoopFEM( se, mesh, ipx, ipw, N, dNdX )
+    #-----------------------------------------------------------------#
+    
+    if USE_DIRECT
+        @time M, b, K, Q, Qt, M0 = SparseAssembly( K_all, Q_all, Mi_all, b_all, mesh, Vx, Vy, P )
+        @time DirectSolveFEM!( M, K, Q, Qt, M0, b, Vx, Vy, P, mesh, b )
+    else
+        nout    = 1000#1e1
+        iterMax = 3e4
+        ϵ_PT    = 1e-7
+        # θ       = 0.11428 *1.7
+        # Δτ      = 0.28 /1.2
+        ΔVxΔτ = zeros(mesh.nn)
+        ΔVyΔτ = zeros(mesh.nn)
+        ΔVxΔτ0= zeros(mesh.nn)
+        ΔVyΔτ0= zeros(mesh.nn)
+        ΔPΔτ  = zeros(mesh.np)
+
+        # PT loop
+        local iter = 0
+        success = 0
+        @time while (iter<iterMax)
+            iter  += 1
+            ΔVxΔτ0 .= ΔVxΔτ 
+            ΔVyΔτ0 .= ΔVyΔτ
+            if USE_NODAL
+                ResidualStokesNodalFEM!( ΔVxΔτ, ΔVyΔτ, ΔPΔτ, Vx, Vy, P, mesh, K_all, Q_all, b_all )
+            else
+                ResidualStokesElementalSerialFEM!( ΔVxΔτ, ΔVyΔτ, ΔPΔτ, Vx, Vy, P, mesh, K_all, Q_all, b_all )
+            end
+            ΔVxΔτ  .= (1.0 - θ).*ΔVxΔτ0 .+ ΔVxΔτ 
+            ΔVyΔτ  .= (1.0 - θ).*ΔVyΔτ0 .+ ΔVyΔτ
+            Vx    .+= ΔτV .* ΔVxΔτ
+            Vy    .+= ΔτV .* ΔVyΔτ
+            P     .+= ΔτP .* ΔPΔτ
+            if iter % nout == 0 || iter==1
+                errVx = norm(ΔVxΔτ)/sqrt(length(ΔVxΔτ))
+                errVy = norm(ΔVyΔτ)/sqrt(length(ΔVyΔτ))
+                errP  = norm(ΔPΔτ) /sqrt(length(ΔPΔτ))
+                @printf("PT Iter. %05d:\n", iter)
+                @printf("  ||Fx|| = %3.3e\n", errVx)
+                @printf("  ||Fy|| = %3.3e\n", errVy)
+                @printf("  ||Fp|| = %3.3e\n", errP )
+                err = max(errVx, errVy, errP)
+                if err < ϵ_PT
+                    print("PT solve converged in ")
+                    success = true
+                    break
+                elseif err>1e4
+                    success = false
+                    println("exploding !")
+                    break
+                elseif isnan(err)
+                    success = false
+                    println("NaN !")
+                    break
+                end
+            end
+        end
+    end
+
+    #-----------------------------------------------------------------#
+    Vxe = zeros(mesh.nel)
+    Vye = zeros(mesh.nel)
+    Ve  = zeros(mesh.nel)
+    Pe  = zeros(mesh.nel)
+    for e=1:mesh.nel
+        for i=1:mesh.nnel
+            Vxe[e] += 1.0/mesh.nnel * Vx[mesh.e2n[e,i]]
+            Vye[e] += 1.0/mesh.nnel * Vy[mesh.e2n[e,i]]
+            Ve[e]  += 1.0/mesh.nnel * sqrt(Vx[mesh.e2n[e,i]]^2 + Vy[mesh.e2n[e,i]]^2)
+        end
+        for i=1:mesh.npel
+            Pe[e] += 1.0/mesh.npel * P[mesh.e2p[e,i]]
+        end
+    end
+    @printf("%2.2e %2.2e\n", minimum(Vx), maximum(Vx))
+    @printf("%2.2e %2.2e\n", minimum(Vy), maximum(Vy))
+    @printf("%2.2e %2.2e\n", minimum(P),  maximum(P) )
+    #-----------------------------------------------------------------#
+    if USE_MAKIE
+        PlotMakie(mesh, Pe, xmin, xmax, ymin, ymax; cmap=:jet1)
+        # PlotMakie(mesh, Pe, xmin, xmax, ymin, ymax; cmap=:jet1)
+    else
+        PlotPyPlot(mesh, Pe, xmin, xmax, ymin, ymax; cmap=:jet1 )
+    end
+end
+
+# main(1, 7, 1, 6, 0.030598470000000003, 0.03666666667,  1.0) # nit = 4000
+# main(2, 7, 1, 6, 0.030598470000000003/2, 0.03666666667,  1.0) # nit = 9000
+main(1, 7, 3, 6, 0.030598470000000003, 0.03666666667,  1.0) # nit = 4000
+
+
+
